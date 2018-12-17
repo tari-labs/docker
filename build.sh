b5498b6d08e7cf58f81f4abb6fe0d02423ca38a3
CjS77 <cayle.sharrock@gmail.com>
2018-12-17 18:02:05 +0200

    Add custom version builds

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-docker build -t quay.io/tarilabs/rust:1.30 ./rust/slim
+VER=${1:-1.30}
+echo $VER
+docker build -t quay.io/tarilabs/rust:$VER ./rust/slim
 docker build -t quay.io/tarilabs/rust:1.30-alpine ./rust/alpine/
 docker build -t quay.io/tarilabs/run:alpine ./run/alpine/
