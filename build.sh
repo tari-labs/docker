7bd726f42bd51eea9445e8e52bd3c67bf4e2bfd8
CjS77 <CjS77@users.noreply.github.com>
2019-03-08 08:58:42 +0200

    Add debug & build tools to tari-build-zmq

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-VER=${1:-1.30}
 echo $VER
-docker build -t quay.io/tarilabs/rust:$VER ./rust/slim
+docker build -t quay.io/tarilabs/rust:1.31 ./rust/slim
 docker build -t quay.io/tarilabs/rust:1.30-alpine ./rust/alpine/
 docker build -t quay.io/tarilabs/run:alpine ./run/alpine/
+# The nightly version
+docker build -t quay.io/tarilabs/rust_tari-build-zmq:nightly-2019-03-08 ./rust/build/
