5224b774a5af50aef42088c436ebea2c3a3204b1
CjS77 <CjS77@users.noreply.github.com>
2019-07-22 13:16:46 +0200

    Change the rust build image name
    Up util a week ago, zmq was the only library dependency required
    to build Tari. Now there are some additional deps, including
    clang and sqlite. The build image is updated to be "with-deps"
    to indicate that all dependent packages are included

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ docker build -t quay.io/tarilabs/rust:1.31 ./rust/slim
 docker build -t quay.io/tarilabs/rust:1.30-alpine ./rust/alpine/
 docker build -t quay.io/tarilabs/run:alpine-1.36 ./run/alpine/
 # The nightly version
-docker build -t quay.io/tarilabs/rust_tari-build-zmq:nightly-2019-07-15 ./rust/build/
+docker build -t quay.io/tarilabs/rust_tari-build-with-deps:nightly-2019-07-15 ./rust/build/
