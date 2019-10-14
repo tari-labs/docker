d911cefe4d9064ffebcaf269b7bced9c0f5db58c
CjS77 <CjS77@users.noreply.github.com>
2019-10-14 10:06:00 +0200

    * Update packages
    - Update base package to Rust:1.38
    - Include C++ libraries for building/linking to C++
    - Update to nighlty 2019-10-04

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ docker build -t quay.io/tarilabs/rust:1.31 ./rust/slim
 docker build -t quay.io/tarilabs/rust:1.30-alpine ./rust/alpine/
 docker build -t quay.io/tarilabs/run:alpine-1.36 ./run/alpine/
 # The nightly version
-docker build -t quay.io/tarilabs/rust_tari-build-with-deps:nightly-2019-09-13 ./rust/build/
+docker build -t quay.io/tarilabs/rust_tari-build-with-deps:nightly-2019-10-04 ./rust/build/
