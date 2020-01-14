9c4f16763c8ec8d3cadc8e7543d704c5c46afe7c
CjS77 <CjS77@users.noreply.github.com>
2020-01-14 13:33:59 +0200

    Bump image versions

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-echo $VER
 docker build -t quay.io/tarilabs/rust:1.31 ./rust/slim
-docker build -t quay.io/tarilabs/rust:1.30-alpine ./rust/alpine/
-docker build -t quay.io/tarilabs/run:alpine-1.36 ./run/alpine/
+docker build -t quay.io/tarilabs/rust:alpine-1.40 ./rust/alpine/
+docker build -t quay.io/tarilabs/run:alpine-1.40 ./run/alpine/
 # The nightly version
-docker build -t quay.io/tarilabs/rust_tari-build-with-deps:nightly-2019-10-04 ./rust/build/
+docker build -t quay.io/tarilabs/rust_tari-build-with-deps:nightly-2020-01-08 ./rust/build/
