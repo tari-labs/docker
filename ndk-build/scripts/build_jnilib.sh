a0ddb34b62a6c5d83c14903d1b463d732793d347
CjS77 <CjS77@users.noreply.github.com>
2020-05-25 10:54:52 +0200

    Pass linker flags to clang

--- ndk-build/scripts/build_jnilib.sh
+++ ndk-build/scripts/build_jnilib.sh
@@ -4,39 +4,72 @@
 # Assume source code resides at GITHUB_WORKSPACE
 PLATFORMABI=$1
 LEVEL=$2
-OUTDIR=/opt/lib/${PLATFORMABI}
 SRCDIR=$3
 
+set -e
 echo "Building ${SRCDIR} for ${PLATFORMABI} on level ${LEVEL}"
 
-# set toolchain path
-TOOLCHAIN=${NDK_PATH}/toolchains/llvm/prebuilt/linux-x86_64
-PLATFORMABI_TOOLCHAIN=${PLATFORMABI}
-PLATFORMABI_COMPILER=${PLATFORMABI}
-if [ "${PLATFORMABI}" = "armv7-linux-androideabi" ]; then \
-    PLATFORMABI_TOOLCHAIN="arm-linux-androideabi"
-    PLATFORMABI_COMPILER="armv7a-linux-androideabi"
-fi
-# set the archiver
-AR=${TOOLCHAIN}/bin/${PLATFORMABI_TOOLCHAIN}'-'ar
-# set the assembler
-AS=${TOOLCHAIN}/bin/${PLATFORMABI_TOOLCHAIN}'-'as
-# set the c and c++ compiler
-CC=${TOOLCHAIN}/bin/${PLATFORMABI_COMPILER}${LEVEL}'-'clang
-CXX=${CC}++
-CXXFLAGS="-stdlib=libstdc++ -isystem ${NDK_PATH}/sources/cxx-stl/llvm-libc++/include"
-echo "CXXFLAGS: ${CXXFLAGS}"
+#PLATFORM=$(cut -d'-' -f1 <<<"${PLATFORMABI}")
+#PLATFORM_OUTDIR=""
+#if [ "${PLATFORM}" == "i686" ]; then
+#  PLATFORM_OUTDIR="x86"
+#  elif [ "${PLATFORM}" == "x86_64" ]; then
+#    PLATFORM_OUTDIR="x86_64"
+#  elif [ "${PLATFORM}" == "armv7" ]; then
+#    PLATFORM_OUTDIR="armeabi-v7a"
+#  elif [ "${PLATFORM}" == "aarch64" ]; then
+#    PLATFORM_OUTDIR="arm64-v8a"
+#  else
+#    PLATFORM_OUTDIR=${PLATFORM}
+#fi
+#
+## set toolchain path
+#TOOLCHAIN=${NDK_PATH}/toolchains/llvm/prebuilt/linux-x86_64
+#PLATFORMABI_TOOLCHAIN=${PLATFORMABI}
+#PLATFORMABI_COMPILER=${PLATFORMABI}
+#if [ "${PLATFORMABI}" = "armv7-linux-androideabi" ]; then \
+#    PLATFORMABI_TOOLCHAIN="arm-linux-androideabi"
+#    PLATFORMABI_COMPILER="armv7a-linux-androideabi"
+#fi
+## set the archiver
+#export AR=${TOOLCHAIN}/bin/${PLATFORMABI_TOOLCHAIN}'-'ar
+## set the assembler
+#export AS=${TOOLCHAIN}/bin/${PLATFORMABI_TOOLCHAIN}'-'as
+## set the c and c++ compiler
+#export CC=${TOOLCHAIN}/bin/${PLATFORMABI_COMPILER}${LEVEL}'-'clang
+#export CXX=${CC}++
+#export CXXFLAGS="-stdlib=libstdc++ -isystem ${NDK_PATH}/sources/cxx-stl/llvm-libc++/include"
+#echo "CXXFLAGS: ${CXXFLAGS}"
+#
+export CFLAGS=${CFLAGS/PF/$PLATFORMABI}
 echo "CFLAGS: ${CFLAGS}"
+#
+export CPPFLAGS=${CPPFLAGS/PF/$PLATFORMABI}
 echo "CPPFLAGS: ${CPPFLAGS}"
-# set the lin
-LD=${TOOLCHAIN}/bin/${PLATFORMABI_TOOLCHAIN}'-'ld
-# set linker flags
-LDFLAGS="-L${TOOLCHAIN}/sysroot/usr/lib/${PLATFORMABI_TOOLCHAIN}/${LEVEL} -L${OUTDIR}/lib ${LDEXTRA}"
-echo "LDFLAGS: ${LDFLAGS}"
-# set the archive index generator tool
-RANLIB=${TOOLCHAIN}/bin/${PLATFORMABI_TOOLCHAIN}'-'ranlib
-# set the symbol stripping tool
-STRIP=${TOOLCHAIN}/bin/${PLATFORMABI_TOOLCHAIN}'-'strip
+
+export RUSTFLAGS=${RUSTFLAGS/PF/$PLATFORMABI}
+echo "RUSTFLAGS: ${RUSTFLAGS}"
+#
+## set the lin
+#export LD=${TOOLCHAIN}/bin/${PLATFORMABI_TOOLCHAIN}'-'ld
+## set linker flags
+## substitute PF as a special variable from LDEXTRA
+#export LDFLAGS="-L${TOOLCHAIN}/sysroot/usr/lib/${PLATFORMABI_TOOLCHAIN}/${LEVEL} ${LDEXTRA/PF/$PLATFORMABI}"
+
+#echo "LDFLAGS: ${LDFLAGS}"
+## set the archive index generator tool
+#export RANLIB=${TOOLCHAIN}/bin/${PLATFORMABI_TOOLCHAIN}'-'ranlib
+## set the symbol stripping tool
+#export STRIP=${TOOLCHAIN}/bin/${PLATFORMABI_TOOLCHAIN}'-'strip
+#
+#echo "Checking compilers.."
+#[[ -f ${AR} ]] && echo "ar: ${AR} is ok"
+#[[ -f ${AS} ]] && echo "as: ${AS} is ok"
+#[[ -f ${CC} ]] && echo "cc: ${CC} is ok"
+#[[ -f ${CXX} ]] && echo "c++: ${CXX} is ok"
+#[[ -f ${LD} ]] && echo "ld: ${LD} is ok"
+#[[ -f ${RANLIB} ]] && echo "ranlib: ${RANLIB} is ok"
+#[[ -f ${STRIP} ]] && echo "strip: ${STRIP} is ok"
 
 cd ${SRCDIR}
 cargo ndk --target ${PLATFORMABI} --android-platform ${LEVEL} -- build ${CARGO_FLAGS}
