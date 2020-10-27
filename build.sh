938a713cd916f6ef49561422a00db68dde874257
CjS77 <CjS77@users.noreply.github.com>
2020-10-27 14:55:05 +0200

    Update build-with-deps
    * Use clang 11
    * Use Rust 1:46 as a base
    * Tag last version (nightly-2020-09-15) that packed_simd will compile on

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ docker build -t quay.io/tarilabs/rust:1.31 ./rust/slim
 docker build -t quay.io/tarilabs/rust:alpine-1.40 ./rust/alpine/
 docker build -t quay.io/tarilabs/run:alpine-1.40 ./run/alpine/
 # The nightly version
-docker build -t quay.io/tarilabs/rust_tari-build-with-deps:nightly-2020-01-08 ./rust/build/
+docker build -t quay.io/tarilabs/rust_tari-build-with-deps:nightly-2020-09-15 ./rust/build/
 # sqlite libraries
 docker build -t quay.io/tarilabs/sqlite-mobile:201911192122 sqlite-mobile/
